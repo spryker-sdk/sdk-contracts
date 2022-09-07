7e4bbf7c80af6a48bc107e77ac39ffb854be318c
Sergey Romankov <sergey.romankov@spryker.com>
2022-09-07 11:27:56 +0300

    SDK-899 update TaskSetInterface

--- src/Entity/TaskSetInterface.php
+++ src/Entity/TaskSetInterface.php
@@ -10,9 +10,39 @@ namespace SprykerSdk\SdkContracts\Entity;
 interface TaskSetInterface extends TaskInterface
 {
     /**
+     * Gets the list of task set tasks.
+     *
      * @param array<string> $tags
      *
      * @return array<\SprykerSdk\SdkContracts\Entity\TaskInterface|string>
      */
     public function getSubTasks(array $tags = []): array;
+
+    /**
+     * Overrides tags of sub-tasks command property. Key is sub-task id.
+     *
+     * @return array<string, array<string>>
+     */
+    public function getTagsMap(): array;
+
+    /**
+     * Overrides stopOnError of sub-tasks command property. Key is sub-task id.
+     *
+     * @return array<string, bool>
+     */
+    public function getStopOnErrorMap(): array;
+
+    /**
+     * Overrides sub-tasks placeholders. Key is sub-task id.
+     *
+     * @return array<string, array<string, array>>
+     */
+    public function getOverridePlaceholdersMap(): array;
+
+    /**
+     * Makes sub-tasks placeholders shared to avoid command input duplication. Key is sub-task id.
+     *
+     * @return array<string, array<string, string>>
+     */
+    public function getSharedPlaceholdersMap(): array;
 }
