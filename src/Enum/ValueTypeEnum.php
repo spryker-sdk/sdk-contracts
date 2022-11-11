a885a0a11a7991357293af1f36d89606bea7ce23
Sergey Romankov <sergey.romankov@spryker.com>
2022-11-11 13:13:16 +0300

    SDK-1110 add ValueTypeEnum getAllValueTypes method

--- src/Enum/ValueTypeEnum.php
+++ src/Enum/ValueTypeEnum.php
@@ -38,4 +38,19 @@ final class ValueTypeEnum
      * @var string
      */
     public const TYPE_UUID = 'uuid';
+
+    /**
+     * @return array<string>
+     */
+    public static function getAllValueTypes(): array
+    {
+        return [
+            self::TYPE_BOOL,
+            self::TYPE_ARRAY,
+            self::TYPE_PATH,
+            self::TYPE_STRING,
+            self::TYPE_INT,
+            self::TYPE_UUID,
+        ];
+    }
 }
